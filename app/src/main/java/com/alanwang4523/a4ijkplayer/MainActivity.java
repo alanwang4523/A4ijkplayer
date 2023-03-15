aff8e64bdb6804bda117d6e0d2d2fc096a8d1259
wangjianjun <wangjianjun@wangjianjundeMacBook-Pro.local>
2023-03-15 18:04:27 +0800

    init demo

---
+++ app/src/main/java/com/alanwang4523/a4ijkplayer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alanwang4523.a4ijkplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
